c5075565a29bf3ad1ca8482fecfa8740d0799e1f
Bowgo Tsai <bowgotsai@google.com>
2024-07-24 22:24:22 +0200

    RMX3741: Add Developer GSI Keys

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/realme/RMX3741
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
